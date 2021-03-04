7075f9c0aad88b58b4c1c2515d48f87126c53dc2
Word Under Pressure DuyThai <wordunderpressureduythai@Words-MacBook-Pro.local>
2021-03-04 23:49:15 +0700

    tutural_chat_socket

---
+++ nodejs/common/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+module.exports = () => {
+    mongoose.connect(
+        'mongodb://localhost:27017/vietpro_mobile_shop',
+        { useNewUrlParser: true }
+    )
+    return mongoose
+}
+
+
